8cf78acdc1c91d2c062f85231e871408eceb9770
Chaz Cheadle <chaz.cheadle@themechanism.com>
2015-04-20 23:36:27 -0400

    Add mobile device accelerometer control. Add quite/escape key functions.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,15 +18,41 @@ public class PlayerController : MonoBehaviour {
 		SetCountText ();
 		winText.text = "";
 	}
-	
+
+	void Update()
+	{
+		if (SystemInfo.deviceType == DeviceType.Desktop) 
+		{
+			// Exit condition for Desktop devices
+			if (Input.GetKey("escape"))
+				Application.Quit();
+		}
+		else
+		{
+			// Exit condition for mobile devices
+			if (Input.GetKeyDown(KeyCode.Escape))
+				Application.Quit();            
+		}
+	}
+
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		if (SystemInfo.deviceType == DeviceType.Desktop) { 
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-		rb.AddForce (movement * speed);
+			rb.AddForce (movement * speed);
+		} else {
+			// Player movement in mobile devices
+			
+			// Building of force vector 
+			Vector3 movement = new Vector3 (Input.acceleration.x, 0.0f, Input.acceleration.y);
+			// Adding force to rigidbody
+			rb.AddForce (movement * speed);
+		}
+		
 	}
 
 	void OnTriggerEnter(Collider other)
